7040c29678da4620482508a24e8a8dc1d8a63fe7
Zisch, Jay <ts-jay.zisch@rakuten.com>
2020-12-07 16:30:03 +0900

    Initial commit

---
+++ Example/testPod1/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  testPod1
+//
+//  Created by Zisch, Jay on 12/07/2020.
+//  Copyright (c) 2020 Zisch, Jay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
